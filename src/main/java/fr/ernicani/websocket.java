136552e3971bbd9361b89704954c59cf3b325a9e
Thomas Lindeker <tlindeker0@gmail.com>
2023-01-30 11:52:49 +0100

    supprimer les truc inutile

--- src/main/java/fr/ernicani/websocket.java
+++ src/main/java/fr/ernicani/websocket.java
@@ -7,18 +7,9 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.messaging.PluginMessageListener;
 
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.BufferedReader;
-import java.lang.reflect.Array;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.Arrays;
 
 public final class websocket extends JavaPlugin implements PluginMessageListener {
 
-    private ServerSocket serverSocket;
-    private Thread listenThread;
 
 
     @Override
@@ -35,13 +26,6 @@ public final class websocket extends JavaPlugin implements PluginMessageListener
     @Override
     public void onDisable() {
         Bukkit.getConsoleSender().sendMessage("§c[WebSocket] Plugin désactivé !");
-        try {
-            serverSocket.close();
-            listenThread.interrupt();
-        } catch (IOException e) {
-            Bukkit.getConsoleSender().sendMessage("§c[WebSocket] Erreur survenue lors de la fermeture du serveur de socket : " + e);
-            e.printStackTrace();
-        }
     }
 
     @Override
@@ -61,7 +45,6 @@ public final class websocket extends JavaPlugin implements PluginMessageListener
         }
     }
 
-    // we check like that if the specified server is BungeeCord.
     private void checkIfBungee()
     {
         if ( !getServer().spigot().getConfig().getConfigurationSection("settings").getBoolean( "settings.bungeecord" ) )
